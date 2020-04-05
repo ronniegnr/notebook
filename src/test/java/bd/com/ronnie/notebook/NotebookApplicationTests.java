b9de0e8f630f0ee9f9b2a7c6f4d51c71fc5110df
ronniegnr <gnr.ronnie@gmail.com>
2020-04-05 19:20:51 +0600

    initial commit

---
+++ src/test/java/bd/com/ronnie/notebook/NotebookApplicationTests.java
@@ -0,0 +1,13 @@
+package bd.com.ronnie.notebook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotebookApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
